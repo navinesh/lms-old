73ec8cd5adf3da46c7eeb0144da16084935359cb
navinesh <navinesh.chand@gmail.com>
2016-05-27 18:06:29 +1200

    refactor code with es6

---
+++ src/leavecalendar.jsx
@@ -0,0 +1,88 @@
+import React from 'react';
+
+var url = 'http://localhost:8080/leave.api';
+
+class Record extends React.Component {
+  render() {
+    return (
+      <tr>
+        <th>
+          {this.props.children}
+        </th>
+      </tr>
+    );
+  }
+}
+
+class RecordList extends React.Component {
+  render() {
+    var itemNodes = this.props.data.map(function(item) {
+      return (
+        <tr key={item.id}>
+          <td>{item.othernames} {item.surname}</td>
+          <td>{item.annual}</td>
+          <td>{item.designation}</td>
+          <td>{item.gender}</td>
+          <td>{item.email}</td>
+        </tr>
+      );
+    });
+    return (
+    <div className="col-sm-8">
+      <table className="table table-bordered table-hover" style={{fontSize: '15px'}}>
+        <thead className="thead-default">
+          <tr>
+            <th>Name</th>
+            <th>Leave type</th>
+            <th>Start date</th>
+            <th>End date</th>
+            <th>Duration</th>
+          </tr>
+        </thead>
+        <tbody>
+          {itemNodes}
+        </tbody>
+      </table>
+    </div>
+    );
+  }
+}
+
+class LeaveCalendar extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {data: {leave_record:[]}}
+  }
+
+  loadRecordsFromServer() {
+      $.ajax({
+        url: url,
+        dataType: 'json',
+        cache: false,
+        success: (data) => {
+          this.setState({data: data});
+        },
+        error: function(xhr, status, err) {
+          console.error(url, status, err.toString());
+        }.bind(this)
+      });
+    }
+
+  componentDidMount() {
+    this.loadRecordsFromServer();
+  }
+
+  componentWillUnmount() {
+    this.loadRecordsFromServer.abort();
+  }
+
+  render() {
+    return (
+      <div className="leaveCalendar">
+        <RecordList data={this.state.data.leave_record} />
+      </div>
+    );
+  }
+}
+
+module.exports = LeaveCalendar;
---
+++ src/login.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+
+class LoginForm extends React.Component{
+  render() {
+    return (
+      <div className="col-sm-4">
+        <div className="card card-block">
+          <form>
+            <fieldset className="form-group">
+              <label for="email">Email address</label>
+              <input type="email" className="form-control" id="email" placeholder="Enter email" />
+            </fieldset>
+            <fieldset className="form-group">
+              <label for="password">Password</label>
+              <input type="password" className="form-control" id="password" placeholder="Password" />
+              <small className="text-muted">Enter your leave management system password.</small>
+            </fieldset>
+            <fieldset className="form-group">
+              <button type="submit" className="btn btn-primary col-sm-12">Log in</button>
+            </fieldset>
+          </form>
+        </div>
+        <a href="/login" className="btn btn-primary-outline col-md-12">Forgot your password?</a>
+      </div>
+    );
+  }
+}
+
+module.exports = LoginForm;
