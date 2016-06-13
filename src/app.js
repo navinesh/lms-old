1e5ae095a4957ad245ae77f2593e484991536526
navinesh <navinesh.chand@gmail.com>
2016-06-14 09:04:06 +1200

    add react-router routes to app

--- src/app.js
+++ src/app.js
@@ -2,14 +2,22 @@ import 'babel-polyfill'
 import React from 'react'
 import { render } from 'react-dom'
 import { Provider } from 'react-redux'
+import { Router, Route, browserHistory } from 'react-router'
 import Main from './containers/main'
 import configureStore from './stores/configureStore'
+import LeaveCalendar from './components/leavecalendar'
+import LoginBox from './components/login'
 
 const store = configureStore()
 
 render(
   <Provider store={store}>
-    <Main />
+    <Router history={browserHistory}>
+      <Route path="/" component={Main}>
+        <Route path="leavecalendar" component={LeaveCalendar} />
+        <Route path="login" component={LoginBox} />
+      </Route>
+    </Router>
   </Provider>,
   document.getElementById('body content')
 )
