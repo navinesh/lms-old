767e73f1b630eaff34be23e481cf7e3259d394be
navinesh <navinesh.chand@gmail.com>
2016-06-17 14:20:02 +1200

    initial commit

---
+++ src/containers/leavecalendarcontainer.js
@@ -0,0 +1,49 @@
+import React, { Component, PropTypes } from 'react'
+import { connect } from 'react-redux'
+import { fetchLeave } from '../actions/actions'
+import LeaveCalendar from '../components/leavecalendar'
+
+var Loader = require('halogen/PulseLoader');
+
+class LeaveCalendarContainer extends Component {
+  constructor(props) {
+    super(props)
+  }
+
+  componentDidMount() {
+    const { dispatch } = this.props
+    dispatch(fetchLeave())
+  }
+
+  render() {
+    const { records, isFetching, lastUpdated } = this.props
+    return (
+      <div className="LeaveCalendarContainer">
+      {(isFetching ? <div className="col-sm-8"><Loader color="#0275d8" size="15px" margin="4px" /></div> : <LeaveCalendar records={records} />)}
+      </div>
+    )
+  }
+}
+
+LeaveCalendarContainer.propTypes = {
+  records: PropTypes.array.isRequired,
+  isFetching: PropTypes.bool.isRequired,
+  lastUpdated: PropTypes.number,
+  dispatch: PropTypes.func.isRequired
+}
+
+const mapStateToProps = (state) => {
+  const {leaveRecords} = state
+  const {
+    isFetching,
+    lastUpdated,
+    items: records
+  } = leaveRecords
+  return {
+    records,
+    isFetching,
+    lastUpdated
+  }
+}
+
+export default connect(mapStateToProps)(LeaveCalendarContainer)
