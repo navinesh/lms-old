c80fdf16f7ff1d496a291a519d6e925d42a31067
navinesh <navinesh.chand@gmail.com>
2016-06-17 14:22:04 +1200

    redefine to include calendar and login components

--- src/containers/main.js
+++ src/containers/main.js
@@ -1,50 +1,14 @@
-import React, { Component, PropTypes } from 'react'
-import { connect } from 'react-redux'
-import { fetchLeave } from '../actions/actions'
-import LeaveCalendar from '../components/leavecalendar'
+import React, { Component } from 'react'
+import LeaveCalendarContainer from './leavecalendarcontainer'
 import LoginBox from '../components/login'
-var Loader = require('halogen/PulseLoader');
 
-class Main extends Component {
-  constructor(props) {
-    super(props)
-  }
-
-  componentDidMount() {
-    const { dispatch } = this.props
-    dispatch(fetchLeave())
-  }
-
+export default class Main extends Component {
   render() {
-    const { records, isFetching, lastUpdated } = this.props
     return (
-      <div className="Main">
-      {(isFetching ? <div className="col-sm-8"><Loader color="#0275d8" size="15px" margin="4px" /></div> : <LeaveCalendar records={records} />)}
-      <LoginBox />
+      <div>
+        <LeaveCalendarContainer />
+        <LoginBox />
       </div>
     )
   }
 }
-
-Main.propTypes = {
-  records: PropTypes.array.isRequired,
-  isFetching: PropTypes.bool.isRequired,
-  lastUpdated: PropTypes.number,
-  dispatch: PropTypes.func.isRequired
-}
-
-function mapStateToProps(state) {
-  const {leaveRecords} = state
-  const {
-    isFetching,
-    lastUpdated,
-    items: records
-  } = leaveRecords
-  return {
-    records,
-    isFetching,
-    lastUpdated
-  }
-}
-
-export default connect(mapStateToProps)(Main)
