bb1d483a0a6889744edb342d1df12e18a7fedc42
navinesh <navinesh.chand@gmail.com>
2016-06-19 12:10:48 +1200

    add user login container

---
+++ src/containers/userlogincontainer.js
@@ -0,0 +1,57 @@
+import React, { Component, PropTypes } from 'react'
+import { connect } from 'react-redux'
+import { fetchLogin } from '../actions/actions'
+import LoginBox from '../components/userlogin'
+
+var Loader = require('halogen/PulseLoader');
+
+class UserLoginBox extends Component {
+  constructor(props) {
+    super(props)
+  }
+
+  render() {
+    const { dispatch, token, message, isFetching, isAuthenticated } = this.props
+
+    return (
+      <div className="UserLoginBox">
+
+      {!isAuthenticated &&
+        <LoginBox
+        message={message}
+        onLoginClick={ creds => dispatch(fetchLogin(creds)) } />
+      }
+
+      {isAuthenticated //&&
+      //  <Logout onLogoutClick={() => dispatch(logoutUser())} />
+      }
+      </div>
+    )
+  }
+}
+
+UserLoginBox.propTypes = {
+  token: PropTypes.string,
+  message: PropTypes.string,
+  isFetching: PropTypes.bool.isRequired,
+  isAuthenticated: PropTypes.bool.isRequired,
+  dispatch: PropTypes.func.isRequired
+}
+
+const mapStateToProps = (state) => {
+  const {userLogin} = state
+  const {
+    isFetching,
+    isAuthenticated,
+    token: token,
+    message
+  } = userLogin
+  return {
+    token,
+    message,
+    isFetching,
+    isAuthenticated
+  }
+}
+
+export default connect(mapStateToProps)(UserLoginBox)
