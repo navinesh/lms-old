6c6f62121bb9fed59bc925bbf2cc0984e338a730
navinesh <navinesh.chand@gmail.com>
2016-06-21 12:38:25 +1200

    add index route

--- src/app.js
+++ src/app.js
@@ -2,8 +2,11 @@ import 'babel-polyfill'
 import React from 'react'
 import { render } from 'react-dom'
 import { Provider } from 'react-redux'
-import { Router, Route, browserHistory } from 'react-router'
+import { Router, Route, browserHistory, IndexRoute } from 'react-router'
+
 import configureStore from './stores/configureStore'
+
+import Header from './containers/header'
 import Main from './containers/main'
 import LeaveCalendarContainer from './containers/leavecalendarcontainer'
 import UserLoginBox from './containers/userlogincontainer'
@@ -13,11 +16,12 @@ const store = configureStore()
 render(
   <Provider store={store}>
     <Router history={browserHistory}>
-      <Route path="/" component={Main}>
+      <Route path="/" component={Header}>
+        <IndexRoute component={Main} />
         <Route path="/leavecalendar" component={LeaveCalendarContainer} />
         <Route path="/login" component={UserLoginBox} />
       </Route>
     </Router>
   </Provider>,
-  document.getElementById('body content')
+  document.getElementById('root')
 )
