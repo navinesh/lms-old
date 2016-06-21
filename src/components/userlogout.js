3f656644037cdeb338cd1ab5481496f246b931c4
navinesh <navinesh.chand@gmail.com>
2016-06-22 11:59:02 +1200

    clean code

--- src/components/userlogout.js
+++ src/components/userlogout.js
@@ -4,11 +4,9 @@ export default class Logout extends Component {
   render(){
     const { onLogoutClick } = this.props
     return(
-      <div className="Logout">
       <button onClick={() => onLogoutClick()} className="btn btn-primary-outline">
         Log out
       </button>
-      </div>
     );
   }
 }
