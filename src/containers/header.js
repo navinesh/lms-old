a7b44354d199034160522e673d0154a4c23777b9
navinesh <navinesh.chand@gmail.com>
2016-06-21 12:46:23 +1200

    initial commit

---
+++ src/containers/header.js
@@ -0,0 +1,44 @@
+import React, { Component, PropTypes } from 'react'
+import { connect } from 'react-redux'
+import { Link } from 'react-router'
+import { logoutUser } from '../actions/userlogoutactions'
+import Logout from '../components/userlogout'
+
+class Header extends Component {
+  render(){
+    const { isAuthenticated, dispatch } = this.props
+    return (
+      <div>
+      <div className="Header">
+        <nav className="navbar navbar-fixed-top">
+          <div className="container">
+          <div className="nav navbar-nav">
+            <a className="nav-item nav-link active" href="/"><h5>Leave management system</h5></a>
+            <a className="nav-item pull-xs-right">
+              {isAuthenticated &&
+                <Logout onLogoutClick={() => dispatch(logoutUser())} />
+              }
+            </a>
+            </div>
+          </div>
+        </nav>
+      </div>
+      <div>
+        {this.props.children}
+      </div>
+    </div>
+    );
+  }
+}
+
+const mapStateToProps = (state) => {
+  const {userAuth} = state
+  const {
+    isAuthenticated,
+  } = userAuth
+  return {
+    isAuthenticated
+  }
+}
+
+export default connect(mapStateToProps)(Header)
