fe4e010e96f4c568814b84fdb9d6d94263f53499
navinesh <navinesh.chand@gmail.com>
2016-06-21 14:05:04 +1200

    initial commit

---
+++ src/containers/userloginbox.js
@@ -0,0 +1,35 @@
+import React, { Component, PropTypes } from 'react'
+import { connect } from 'react-redux'
+import LeaveCalendarContainer from './leavecalendarcontainer'
+import UserLoginBox from './userlogincontainer'
+import Header from './header'
+
+class MainUserLoginBox extends Component {
+  render() {
+    const { isAuthenticated } = this.props
+
+    return (
+      <div className="MainView">
+        {!isAuthenticated &&
+          <div className="row">
+            <div className="col-sm-offset-4 col-sm-4">
+              <UserLoginBox />
+            </div>
+          </div>
+        }
+      </div>
+    )
+  }
+}
+
+const mapStateToProps = (state) => {
+  const {userAuth} = state
+  const {
+    isAuthenticated,
+  } = userAuth
+  return {
+    isAuthenticated
+  }
+}
+
+export default connect(mapStateToProps)(MainUserLoginBox)
