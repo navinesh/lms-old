f018289ed8feee75f98f368c110974ca9a60c2fb
navinesh <navinesh.chand@gmail.com>
2016-06-22 16:58:40 +1200

    add leave calendar link and change signout link

--- src/containers/header.js
+++ src/containers/header.js
@@ -2,7 +2,7 @@ import React, { Component, PropTypes } from 'react'
 import { connect } from 'react-redux'
 import { Link } from 'react-router'
 import { logoutUser } from '../actions/userlogoutactions'
-import Logout from '../components/userlogout'
+import LeaveCalendarContainer from './leavecalendarcontainer'
 
 class Header extends Component {
   render(){
@@ -12,12 +12,17 @@ class Header extends Component {
         <nav className="navbar navbar-fixed-top">
           <div className="container">
             <div className="nav navbar-nav">
-              <a className="nav-item nav-link active" href="/"><h5>Leave management system</h5></a>
-              <a className="nav-item nav-link pull-xs-right">
-                {isAuthenticated &&
-                  <Logout onLogoutClick={() => dispatch(logoutUser())} />
-                }
-              </a>
+              <Link className="nav-item nav-link active" to="/"><h5>Leave management system</h5></Link>
+              {isAuthenticated &&
+                <button className="btn btn-link pull-xs-right" onClick={() => dispatch(logoutUser())}>
+                  Sign out
+                </button>
+              }
+              {isAuthenticated &&
+                <Link className="nav-item nav-link pull-xs-right" to="/leavecalendar">
+                  Leave calendar
+                </Link>
+              }
             </div>
           </div>
         </nav>
