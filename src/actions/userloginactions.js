3c604e82b5726cd1e5e8c28473409833228737ad
navinesh <navinesh.chand@gmail.com>
2016-06-29 16:37:51 +1200

    add token actions

--- src/actions/userloginactions.js
+++ src/actions/userloginactions.js
@@ -4,6 +4,10 @@ export const LOGIN_USER_REQUEST = 'LOGIN_USER_REQUEST'
 export const LOGIN_USER_SUCCESS = 'LOGIN_USER_SUCCESS'
 export const LOGIN_USER_FAILURE = 'LOGIN_USER_FAILURE'
 
+export const LOGIN_USER_REQUEST_FROM_TOKEN = 'LOGIN_USER_REQUEST_FROM_TOKEN'
+export const LOGIN_USER_SUCCESS_FROM_TOKEN = 'LOGIN_USER_SUCCESS_FROM_TOKEN'
+export const LOGIN_USER_FAILURE_FROM_TOKEN = 'LOGIN_USER_FAILURE_FROM_TOKEN'
+
 export function requestUserLogin(creds){
   return {
     type: LOGIN_USER_REQUEST,
@@ -13,15 +17,33 @@ export function requestUserLogin(creds){
 
 export function receiveUserLogin(data){
   return {
-    type: LOGIN_USER_SUCCESS,
-    auth_token: data.auth_token,
-    user_id: data.user_id
+    type: LOGIN_USER_SUCCESS
   }
 }
 
 export function loginUserError(data){
   return {
     type: LOGIN_USER_FAILURE,
+    message: data.message
+  }
+}
+
+export function requestUserLoginFromToken(auth_token){
+  return {
+    type: LOGIN_USER_REQUEST_FROM_TOKEN,
+    auth_token
+  }
+}
+
+export function receiveUserLoginFromToken(data){
+  return {
+    type: LOGIN_USER_SUCCESS_FROM_TOKEN
+  }
+}
+
+export function loginUserErrorFromToken(data){
+  return {
+    type: LOGIN_USER_FAILURE_FROM_TOKEN,
     message: data.message
   }
 }
@@ -40,7 +62,26 @@ export function fetchLogin(creds) {
         else {
           localStorage.setItem('auth_token', response.data.auth_token)
           localStorage.setItem('user_id', response.data.user_id)
-          dispatch(receiveUserLogin(response.data))
+          dispatch(receiveUserLogin())
+        }
+      })
+  }
+}
+
+export function fetchLoginFromToken(auth_token) {
+  return dispatch => {
+    dispatch(requestUserLoginFromToken(auth_token))
+    axios.post('usertoken', {
+        auth_token: auth_token
+      })
+      .then((response) => {
+        if (response.status === 200){
+          dispatch(loginUserErrorFromToken(response.data))
+          localStorage.removeItem('auth_token')
+          localStorage.removeItem('user_id')
+        }
+        else {
+          dispatch(receiveUserLoginFromToken())
         }
       })
   }
