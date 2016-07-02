8aaa188c988b10108cb5914392bd90c20f7d3111
navinesh <navinesh.chand@gmail.com>
2016-07-03 00:49:26 +1200

    clean code

--- src/containers/userloginbox.js
+++ src/containers/userloginbox.js
@@ -2,7 +2,6 @@ import React, { Component, PropTypes } from 'react'
 import { connect } from 'react-redux'
 import LeaveCalendarContainer from './leavecalendarcontainer'
 import UserLoginBox from './userlogincontainer'
-import Header from './header'
 
 class MainUserLoginBox extends Component {
   render() {
