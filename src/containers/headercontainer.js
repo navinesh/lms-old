c63f0fb44cae22427bf549977a56a76c0cf792d3
navinesh <navinesh.chand@gmail.com>
2016-07-04 15:21:25 +1200

    fix componentDidMount function

--- src/containers/headercontainer.js
+++ src/containers/headercontainer.js
@@ -1,23 +1,17 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
-import { Link } from 'react-router'
-import { fetchLoginFromToken } from '../actions/userloginactions'
 import Header from '../components/header'
+import { fetchLoginFromToken } from '../actions/userloginactions'
 import { logoutUser } from '../actions/userlogoutactions'
-import LeaveCalendarContainer from './leavecalendarcontainer'
 
 class Headercontainer extends Component {
-  //componentDidMount() {
-    //this.loadToken()
-  //}
-
-  loadToken() {
-   const { dispatch } = this.props
-   let auth_token = localStorage.getItem('auth_token')
-   if(!auth_token || auth_token === '') {
-     return;
-   }
-     dispatch(fetchLoginFromToken(auth_token))
+  componentDidMount() {
+    const { dispatch } = this.props
+    let auth_token = localStorage.getItem('auth_token')
+    if(!auth_token || auth_token === '') {
+      return;
+    }
+      dispatch(fetchLoginFromToken(auth_token))
   }
 
   render(){
