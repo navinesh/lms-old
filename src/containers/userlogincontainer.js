5a4b7abd24e4a2bbb4cdeac03d03b5f718d69a26
navinesh <navinesh.chand@gmail.com>
2016-08-01 16:10:47 +1200

    remove import PropTypes

--- src/containers/userlogincontainer.js
+++ src/containers/userlogincontainer.js
@@ -1,4 +1,4 @@
-import React, { Component, PropTypes } from 'react'
+import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { fetchLogin } from '../actions/userloginactions'
 import { logoutUser } from '../actions/userlogoutactions'
