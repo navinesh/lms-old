da028b79283b8c73cbfef5bdd152b38a1be7886c
navinesh <navinesh.chand@gmail.com>
2016-08-08 21:27:21 +1200

    add header type

--- src/components/usererror.js
+++ src/components/usererror.js
@@ -5,10 +5,8 @@ export default class UserError extends Component {
     return(
       <div className="container text-xs-center">
         <div className="offset-sm-2 col-sm-8">
-          <h1>404</h1>
-          <p>
-            <h3>That's an error.</h3>
-          </p>
+          <h2>404</h2>
+            <h4>That's an error.</h4>
           <h5>The site configured at this address does not contain the requested file.</h5>
         </div>
       </div>
