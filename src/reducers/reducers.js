851a1d674001a99dc15b6fb05910d31546db1181
navinesh <navinesh.chand@gmail.com>
2016-08-08 15:48:50 +1200

    add user details reducer

--- src/reducers/reducers.js
+++ src/reducers/reducers.js
@@ -5,6 +5,7 @@ import { LOGIN_USER_REQUEST, LOGIN_USER_SUCCESS, LOGIN_USER_FAILURE,
         LOGIN_USER_REQUEST_FROM_TOKEN, LOGIN_USER_SUCCESS_FROM_TOKEN,
         LOGIN_USER_FAILURE_FROM_TOKEN } from '../actions/userloginactions'
 import { LOGOUT_USER_SUCCESS } from '../actions/userlogoutactions'
+import { REQUEST_USER_DETAILS, RECEIVE_USER_DETAILS, USER_DETAILS_ERROR } from '../actions/userdetailsactions'
 
 function leaveRecords(state = {isFetching: false,
   items: []}, action) {
@@ -63,9 +64,29 @@ function userAuth(state = {isFetching: false,
   }
 }
 
+function userDetails(state = {isFetching: false,
+  userDetail: []}, action) {
+  switch (action.type) {
+    case REQUEST_USER_DETAILS:
+    return { ...state,
+      isFetching: true}
+    case RECEIVE_USER_DETAILS:
+    return { ...state,
+      isFetching: false,
+      userDetail: action.user_detail}
+    case USER_DETAILS_ERROR:
+    return { ...state,
+      isFetching: false,
+      message: action.message}
+    default:
+      return state
+  }
+}
+
 const rootReducer = combineReducers({
   leaveRecords,
-  userAuth
+  userAuth,
+  userDetails
 })
 
 export default rootReducer
