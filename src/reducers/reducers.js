9c0fc725f21a2ac8f2bfd6352baf21a3ec9221cf
navinesh <navinesh.chand@gmail.com>
2016-08-25 14:20:08 +1200

    add leave application and password reset functions

--- src/reducers/reducers.js
+++ src/reducers/reducers.js
@@ -1,11 +1,21 @@
 import { combineReducers } from 'redux'
 
 import { REQUEST_LEAVE_CALENDAR, RECEIVE_LEAVE_CALENDAR } from '../actions/leavecalendar'
+
 import { LOGIN_USER_REQUEST, LOGIN_USER_SUCCESS, LOGIN_USER_FAILURE,
         LOGIN_USER_REQUEST_FROM_TOKEN, LOGIN_USER_SUCCESS_FROM_TOKEN,
         LOGIN_USER_FAILURE_FROM_TOKEN } from '../actions/userlogin'
+
 import { LOGOUT_USER_SUCCESS } from '../actions/userlogout'
-import { REQUEST_USER_DETAILS, RECEIVE_USER_DETAILS, USER_DETAILS_ERROR } from '../actions/userdetails'
+
+import { REQUEST_USER_DETAILS, RECEIVE_USER_DETAILS,
+        USER_DETAILS_ERROR } from '../actions/userdetails'
+
+import { LEAVE_APPLICATION_REQUEST, LEAVE_APPLICATION_SUCCESS,
+        LEAVE_APPLICATION_FAILURE } from '../actions/leaveapplication'
+
+import { REQUEST_PASSWORD_RESET, PASSWORD_RESET_SUCCESS,
+        PASSWORD_RESET_ERROR } from '../actions/resetpassword'
 
 function leaveRecords(state = {isFetching: false,
   items: []}, action) {
@@ -83,10 +93,51 @@ function userDetails(state = {isFetching: false,
   }
 }
 
+function leaveApplication (state = {isFetching: false,
+  message: ''}, action) {
+  switch (action.type) {
+    case LEAVE_APPLICATION_REQUEST:
+    return { ...state,
+      isFetching: true,
+      message: ''}
+    case LEAVE_APPLICATION_SUCCESS:
+    return { ...state,
+      isFetching: false,
+      message: action.message}
+    case LEAVE_APPLICATION_FAILURE:
+    return { ...state,
+      isFetching: false,
+      message: action.message}
+    default:
+    return state
+  }
+}
+
+function resetPassword(state = {isFetching: false,
+  message: ''}, action) {
+  switch (action.type) {
+    case REQUEST_PASSWORD_RESET:
+    return { ...state,
+      isFetching: true}
+    case PASSWORD_RESET_SUCCESS:
+    return { ...state,
+      isFetching: false,
+      message: action.message}
+    case PASSWORD_RESET_ERROR:
+    return { ...state,
+      isFetching: false,
+      message: action.message}
+    default:
+      return state
+  }
+}
+
 const rootReducer = combineReducers({
   leaveRecords,
   userAuth,
-  userDetails
+  userDetails,
+  leaveApplication,
+  resetPassword
 })
 
 export default rootReducer
