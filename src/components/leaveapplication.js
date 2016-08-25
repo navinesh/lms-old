891608395086a933f10d5c7da8d6878b147aa8e9
navinesh <navinesh.chand@gmail.com>
2016-08-26 10:07:17 +1200

    delete condition to check if the form data has been submitted successfully

--- src/components/leaveapplication.js
+++ src/components/leaveapplication.js
@@ -1,11 +1,10 @@
 import React, { Component, PropTypes } from 'react'
-import { Link } from 'react-router'
 var Loader = require('halogen/ClipLoader');
 
 export default class LeaveApplications extends Component {
   constructor() {
     super()
-    this.state = { errorMessage: '', successMessage: '' };
+    this.state = { errorMessage: '' };
     this.handleLeaveChange = this.handleLeaveChange.bind(this);
     this.handleLeaveTypeChange = this.handleLeaveTypeChange.bind(this);
     this.handleDateFromChange = this.handleDateFromChange.bind(this);
@@ -61,7 +60,6 @@ export default class LeaveApplications extends Component {
     }
 
     this.setState({errorMessage: ''});
-    this.setState({successMessage: 'Your application has been submitted.'});
 
     const applicationDetails = { user_id: user_id, leave: leave, leaveType: leaveType,
       dateFrom: dateFrom, dateTo: dateTo, supervisorEmail: supervisorEmail,
@@ -70,97 +68,85 @@ export default class LeaveApplications extends Component {
   }
 
   render(){
-    const { isFetching, message } = this.props
-    if (this.state.successMessage) {
-      return(
-        <div className="container text-xs-center" style={{paddingTop: '100px'}}>
-          <div className="col-sm-12">
-            <h1 className="display-4">{this.state.successMessage}</h1>
+    return(
+      <div className="col-xs-12 col-sm-6 offset-sm-3">
+        <div className="card card-block">
+          <form onSubmit={this.handleSubmit}>
+            <fieldset className="form-group">
+              <label for="leave">Leave</label>
+              <select className="form-control" id="leave" onChange={this.handleLeaveChange}>
+                <option></option>
+                <option>annual</option>
+                <option>sick</option>
+                <option>bereavement</option>
+                <option>christmas</option>
+                <option>birthday</option>
+                <option>maternity</option>
+                <option>lwop</option>
+                <option>other</option>
+              </select>
+            </fieldset>
+            <fieldset className="form-group">
+              <label for="leaveType">Leave type</label>
+              <select className="form-control" id="leaveType" onChange={this.handleLeaveTypeChange}>
+                <option></option>
+                <option>full</option>
+                <option>half day am</option>
+                <option>half day pm</option>
+              </select>
+            </fieldset>
+            <fieldset className="form-group">
+              <label for="dateFrom">From</label>
+              <input type="date" className="form-control"
+                placeholder="month/day/year" id="dateFrom"
+                onChange={this.handleDateFromChange} />
+            </fieldset>
+            <fieldset className="form-group">
+              <label for="dateTo">To</label>
+              <input type="date" className="form-control"
+                placeholder="month/day/year" id="dateTo"
+                onChange={this.handleDateToChange} />
+            </fieldset>
+            <fieldset className="form-group">
+              <label for="supervisorEmail">Supervisor email</label>
+              <input type="email" className="form-control"
+                placeholder="Supervisor email" id="supervisorEmail"
+                onChange={this.handleSupervisorEmailChange} />
+            </fieldset>
+            <fieldset className="form-group">
+              <label for="secretaryEmail">Second supervisor / secretary email</label>
+              <input type="email" className="form-control"
+                placeholder="Second supervisor / secretary email" id="secretaryEmail"
+                onChange={this.handleSecretaryEmailChange} />
+            </fieldset>
+            <fieldset className="form-group">
+              <label for="reason">Reason</label>
+              <input type="text" className="form-control"
+                placeholder="Reason for leave" id="reason"
+                onChange={this.handleReasonChange} />
+            </fieldset>
+            <fieldset className="form-group">
+              <label for="sicksheet">Sick sheet</label>
+              <input type="file" className="form-control-file" id="sicksheet" />
+              <small className="text-muted">A medical certificate is required for absence of two consecutive days or more and after four single day absences.</small>
+            </fieldset>
+            <fieldset className="form-group">
+              <button type="submit" className="btn btn-primary col-xs-12 col-sm-12">Submit</button>
+            </fieldset>
+          </form>
+          <div className="text-danger text-xs-center">
+            {this.props.isFetching ?
+              <Loader color="#0275d8" size="20px" />:
+              this.props.message}
+          </div>
+          <div className="text-danger text-xs-center">
+            {this.state.errorMessage ?
+              <div>{this.state.errorMessage}</div> :
+                 ''}
           </div>
         </div>
-      );
-    }
-    else {
-      return(
-        <div className="col-xs-12 col-sm-6 offset-sm-3">
-          <div className="card card-block">
-            <form onSubmit={this.handleSubmit}>
-              <fieldset className="form-group">
-                <label for="leave">Leave</label>
-                <select className="form-control" id="leave" onChange={this.handleLeaveChange}>
-                  <option></option>
-                  <option>annual</option>
-                  <option>sick</option>
-                  <option>bereavement</option>
-                  <option>christmas</option>
-                  <option>birthday</option>
-                  <option>maternity</option>
-                  <option>lwop</option>
-                  <option>other</option>
-                </select>
-              </fieldset>
-              <fieldset className="form-group">
-                <label for="leaveType">Leave type</label>
-                <select className="form-control" id="leaveType" onChange={this.handleLeaveTypeChange}>
-                  <option></option>
-                  <option>full</option>
-                  <option>half day am</option>
-                  <option>half day pm</option>
-                </select>
-              </fieldset>
-              <fieldset className="form-group">
-                <label for="dateFrom">From</label>
-                <input type="date" className="form-control"
-                  placeholder="month/day/year" id="dateFrom"
-                  onChange={this.handleDateFromChange} />
-              </fieldset>
-              <fieldset className="form-group">
-                <label for="dateTo">To</label>
-                <input type="date" className="form-control"
-                  placeholder="month/day/year" id="dateTo"
-                  onChange={this.handleDateToChange} />
-              </fieldset>
-              <fieldset className="form-group">
-                <label for="supervisorEmail">Supervisor email</label>
-                <input type="email" className="form-control"
-                  placeholder="Supervisor email" id="supervisorEmail"
-                  onChange={this.handleSupervisorEmailChange} />
-              </fieldset>
-              <fieldset className="form-group">
-                <label for="secretaryEmail">Second supervisor / secretary email</label>
-                <input type="email" className="form-control"
-                  placeholder="Second supervisor / secretary email" id="secretaryEmail"
-                  onChange={this.handleSecretaryEmailChange} />
-              </fieldset>
-              <fieldset className="form-group">
-                <label for="reason">Reason</label>
-                <input type="text" className="form-control"
-                  placeholder="Reason for leave" id="reason"
-                  onChange={this.handleReasonChange} />
-              </fieldset>
-              <fieldset className="form-group">
-                <label for="sicksheet">Sick sheet</label>
-                <input type="file" className="form-control-file" id="sicksheet" />
-                <small className="text-muted">A medical certificate is required for absence of two consecutive days or more and after four single day absences.</small>
-              </fieldset>
-              <fieldset className="form-group">
-                <button type="submit" className="btn btn-primary col-xs-12 col-sm-12">Submit</button>
-              </fieldset>
-            </form>
-            <div className="text-danger text-xs-center">
-              {isFetching ?
-                <Loader color="#0275d8" size="20px" />:
-                message}
-            </div>
-            <div className="text-danger text-xs-center">
-              {this.state.errorMessage ?
-                <div>{this.state.errorMessage}</div> :
-                   ''}
-            </div>
-          </div>
-        </div>
-      );
-    }
+      </div>
+    );
   }
 }
 
