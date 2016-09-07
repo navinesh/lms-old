5cfa6246c72c8ba31f31b611913538e42d406538
navinesh <navinesh.chand@gmail.com>
2016-09-08 11:56:34 +1200

    use arrow functions

--- src/actions/leavecalendar.js
+++ src/actions/leavecalendar.js
@@ -3,19 +3,15 @@ import fetch from 'isomorphic-fetch'
 export const REQUEST_LEAVE_CALENDAR = 'REQUEST_LEAVE_CALENDAR'
 export const RECEIVE_LEAVE_CALENDAR = 'RECEIVE_LEAVE_CALENDAR'
 
-export function requestLeave() {
-  return {
-    type: REQUEST_LEAVE_CALENDAR,
-  }
-}
+export const requestLeave = () => ({
+  type: REQUEST_LEAVE_CALENDAR
+});
 
-export function receiveLeave(json) {
-  return {
-    type: RECEIVE_LEAVE_CALENDAR,
-    records: json.leave_records,
-    receivedAt: Date.now()
-  }
-}
+export const receiveLeave = (json) => ({
+  type: RECEIVE_LEAVE_CALENDAR,
+  records: json.leave_records,
+  receivedAt: Date.now()
+});
 
 export function fetchLeave() {
   return dispatch => {
