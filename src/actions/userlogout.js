4dbb492f43b1fac08562ca6ea5c01f4813e067b5
navinesh <navinesh.chand@gmail.com>
2016-09-08 12:03:38 +1200

    use arrow functions

--- src/actions/userlogout.js
+++ src/actions/userlogout.js
@@ -1,22 +1,17 @@
 export const LOGOUT_USER_REQUEST = 'LOGOUT_USER_REQUEST'
 export const LOGOUT_USER_SUCCESS = 'LOGOUT_USER_SUCCESS'
 
+export const requestLogout = () => ({
+  type: LOGOUT_USER_REQUEST,
+  isFetching: true,
+  isAuthenticated: true
+});
 
-function requestLogout() {
-  return {
-    type: LOGOUT_USER_REQUEST,
-    isFetching: true,
-    isAuthenticated: true
-  }
-}
-
-function receiveLogout() {
-  return {
-    type: LOGOUT_USER_SUCCESS,
-    isFetching: false,
-    isAuthenticated: false
-  }
-}
+export const receiveLogout = () => ({
+  type: LOGOUT_USER_SUCCESS,
+  isFetching: false,
+  isAuthenticated: false
+});
 
 export function logoutUser() {
   return dispatch => {
