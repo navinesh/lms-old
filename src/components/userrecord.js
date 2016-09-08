7a2faa563565be559425e59f7cdd215ea185d452
navinesh <navinesh.chand@gmail.com>
2016-09-09 07:14:18 +1200

    initial commit

---
+++ src/components/userrecord.js
@@ -0,0 +1,211 @@
+import React, { Component, PropTypes } from 'react'
+
+class PendingRecordList extends Component {
+  render() {
+    const { user_record } = this.props
+    const userItems = []
+    for (let x = 0; x < user_record.length; x++) {
+      user_record[x].leaverecord.map((record) => {
+        if (record.leave_status === 'pending') {
+          userItems.push(<tr key={record.id}>
+                      <td>{record.leave_name}</td>
+                      <td>{record.leave_days} day(s)</td>
+                      <td>{record.start_date}</td>
+                      <td>{record.end_date}</td>
+                      <td>{record.leave_reason}</td>
+                    </tr>)
+        }
+        return (userItems)
+      });
+    }
+    if (userItems.length > 0){
+      return (
+        <div>
+        <h6>PENDING LEAVE SCHEDULE</h6>
+        <table className="table table-bordered table-hover">
+          <thead className="thead-default">
+            <tr>
+              <th>Leave type</th>
+              <th>Leave days</th>
+              <th>Start date</th>
+              <th>End date</th>
+              <th>Reason</th>
+            </tr>
+          </thead>
+          <tbody>
+          {userItems}
+          </tbody>
+        </table>
+      </div>
+      );
+    }
+    else {
+      return (<div />);
+    }
+  }
+}
+
+class ApprovedRecordList extends Component {
+  render() {
+    const { user_record } = this.props
+    const userItems = []
+    for (let x = 0; x < user_record.length; x++) {
+      user_record[x].leaverecord.map((record) => {
+        if (record.leave_status === 'approved') {
+          userItems.push(<tr key={record.id}>
+                      <td>{record.leave_name}</td>
+                      <td>{record.leave_days} day(s)</td>
+                      <td>{record.start_date}</td>
+                      <td>{record.end_date}</td>
+                      <td>{record.leave_reason}</td>
+                    </tr>)
+        }
+        return (userItems)
+      });
+    }
+    if (userItems.length > 0){
+      return (
+        <div>
+        <h6>APPROVED LEAVE SCHEDULE</h6>
+        <table className="table table-bordered table-hover">
+          <thead className="thead-default">
+            <tr>
+              <th>Leave type</th>
+              <th>Leave days</th>
+              <th>Start date</th>
+              <th>End date</th>
+              <th>Reason</th>
+            </tr>
+          </thead>
+          <tbody>
+          {userItems}
+          </tbody>
+        </table>
+      </div>
+      );
+    }
+    else {
+      return (<div />);
+    }
+  }
+}
+
+class UserRecordList extends Component {
+  render() {
+    const { user_record, message } = this.props
+    if(message){
+      return (
+        <div className="container text-xs-center" style={{paddingTop: '100px'}}>
+          <div className="offset-sm-2 col-sm-8">
+            <h1 className="display-4">The site configured at this address does not contain the requested resource.</h1>
+          </div>
+        </div>
+      );
+    }
+
+    const userItem = user_record.map((user) => {
+      if(user.maternity) {
+        return (
+          <div key={user.id}>
+            <div className="row">
+              <h5 className="card-title col-sm-12">{user.othernames} {user.surname}</h5>
+              <div className="col-sm-3">
+                <div className="card card-block text-xs-center">
+                  <h3 className="card-title">{user.annual}</h3>
+                  <p className="card-text">Annual</p>
+                </div>
+              </div>
+              <div className="col-sm-2">
+                <div className="card card-block text-xs-center">
+                  <h3 className="card-title">{user.sick}</h3>
+                  <p className="card-text">Sick</p>
+                </div>
+              </div>
+              <div className="col-sm-2">
+                <div className="card card-block text-xs-center">
+                  <h3 className="card-title">{user.bereavement}</h3>
+                  <p className="card-text">Bereavement</p>
+                </div>
+              </div>
+              <div className="col-sm-2">
+                <div className="card card-block text-xs-center">
+                  <h3 className="card-title">{user.christmas}</h3>
+                  <p className="card-text">Christmas</p>
+                </div>
+              </div>
+              <div className="col-sm-3">
+                <div className="card card-block text-xs-center">
+                  <h3 className="card-title">{user.maternity}</h3>
+                  <p className="card-text">Maternity</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        );
+      }
+      else {
+        return (
+          <div key={user.id}>
+            <div className="row">
+              <h5 className="card-title col-sm-12">{user.othernames} {user.surname}</h5>
+              <div className="col-sm-3">
+                <div className="card card-block text-xs-center">
+                  <h3 className="card-title">{user.annual}</h3>
+                  <p className="card-text">Annual</p>
+                </div>
+              </div>
+              <div className="col-sm-3">
+                <div className="card card-block text-xs-center">
+                  <h3 className="card-title">{user.sick}</h3>
+                  <p className="card-text">Sick</p>
+                </div>
+              </div>
+              <div className="col-sm-3">
+                <div className="card card-block text-xs-center">
+                  <h3 className="card-title">{user.bereavement}</h3>
+                  <p className="card-text">Bereavement</p>
+                </div>
+              </div>
+              <div className="col-sm-3">
+                <div className="card card-block text-xs-center">
+                  <h3 className="card-title">{user.christmas}</h3>
+                  <p className="card-text">Christmas</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        );
+      }
+    });
+    return (
+      <div>
+      {userItem}
+      </div>
+    );
+  }
+}
+
+class UserRecord extends Component {
+  render() {
+    return (
+      <div className="UserRecord">
+        <UserRecordList user_record={this.props.user_record} message={this.props.message} />
+        {/*<div className="row">*/}
+          <div className="col-sm-12">
+            <PendingRecordList user_record={this.props.user_record} />
+          </div>
+          <div className="col-sm-12">
+            <ApprovedRecordList user_record={this.props.user_record} />
+          </div>
+        {/*</div>*/}
+      </div>
+    );
+  }
+}
+
+UserRecord.propTypes = {
+  user_record: PropTypes.array.isRequired,
+  message: PropTypes.string
+}
+
+export default UserRecord;
---
+++ src/containers/userrecord.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { fetchUserRecord } from '../actions/userrecord'
+import UserRecord from '../components/userrecord'
+
+var Loader = require('halogen/PulseLoader');
+
+class UserRecords extends Component {
+
+  componentDidMount() {
+    const { dispatch } = this.props
+    const user_id = localStorage.getItem('user_id')
+    dispatch(fetchUserRecord(user_id))
+  }
+
+  render() {
+    const { isFetching, user_record, message } = this.props
+    return (
+      <div className="UserDetailsContainer">
+      {(isFetching ? <div className="offset-sm-5">
+      <Loader color="#0275d8" size="12px" /></div> :
+      <UserRecord user_record={user_record} message={message} />)}
+      </div>
+    )
+  }
+}
+
+const mapStateToProps = (state) => {
+  const {userRecords} = state
+  const {
+    isFetching,
+    userRecord: user_record,
+    message
+  } = userRecords
+  return {
+    isFetching,
+    user_record,
+    message
+  }
+}
+
+export default connect(mapStateToProps)(UserRecords)
