4a5511f612a37b2da8ff7859687855138d6111ef
navinesh <navinesh.chand@gmail.com>
2016-09-13 15:31:15 +1200

    rewrite es6 class as stateless functional component

--- src/components/header.js
+++ src/components/header.js
@@ -1,47 +1,46 @@
-import React, { Component, PropTypes } from 'react'
+import React, { PropTypes } from 'react'
 import { Link } from 'react-router'
 
-export default class Navs extends Component {
-  render(){
-    const { isAuthenticated, onLogoutClick, children } = this.props
-    return (
-      <div className="Navs">
-        <nav className="navbar navbar-fixed-top">
-          <div className="container">
-            {!isAuthenticated &&
-              <div className="nav navbar-nav">
-                <Link className="nav-item nav-link" to="/">
-                  Leave management system
+const Navs = ({ isAuthenticated, onLogoutClick, children }) => {
+  return (
+    <div className="Navs">
+      <nav className="navbar navbar-fixed-top">
+        <div className="container">
+          {!isAuthenticated &&
+            <div className="nav navbar-nav">
+              <Link className="nav-item nav-link" to="/">
+                Leave management system
+              </Link>
+            </div>
+          }
+          {isAuthenticated &&
+            <div className="nav navbar-nav">
+              <Link className="nav-item nav-link" to="/">
+                Leave management system
+              </Link>
+              <div className="pull-xs-right">
+                <Link className="nav-item nav-link" to="/leaveapplication">
+                  Leave application
                 </Link>
-              </div>
-            }
-            {isAuthenticated &&
-              <div className="nav navbar-nav">
-                <Link className="nav-item nav-link" to="/">
-                  Leave management system
+                <Link className="nav-item nav-link" to="/leavecalendar">
+                  Leave calendar
                 </Link>
-                <div className="pull-xs-right">
-                  <Link className="nav-item nav-link" to="/leaveapplication">
-                    Leave application
-                  </Link>
-                  <Link className="nav-item nav-link" to="/leavecalendar">
-                    Leave calendar
-                  </Link>
-                  <button onClick={() => onLogoutClick()} className="btn btn-primary btn-right">
-                    Sign out
-                  </button>
-                </div>
+                <button onClick={() => onLogoutClick()} className="btn btn-primary btn-right">
+                  Sign out
+                </button>
               </div>
-            }
-          </div>
-        </nav>
-        {children}
-      </div>
-    );
-  }
-}
+            </div>
+          }
+        </div>
+      </nav>
+      {children}
+    </div>
+  );
+};
 
 Navs.propTypes = {
   onLogoutClick: PropTypes.func.isRequired,
   isAuthenticated: PropTypes.bool.isRequired
 }
+
+export default Navs
