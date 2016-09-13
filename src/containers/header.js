d22d6175e7deda68a8c832ae4d6d2545dbd9253d
navinesh <navinesh.chand@gmail.com>
2016-09-13 15:31:54 +1200

    rewrite es6 class as stateless functional component

--- src/containers/header.js
+++ src/containers/header.js
@@ -1,38 +1,22 @@
-import React, { Component } from 'react'
+import React from 'react'
 import { connect } from 'react-redux'
 import Navs from '../components/header'
-import { fetchLoginFromToken } from '../actions/userlogin'
 import { logoutUser } from '../actions/userlogout'
 
-class Header extends Component {
-  componentDidMount() {
-    const { dispatch } = this.props
-    const auth_token = localStorage.getItem('auth_token')
-    if(!auth_token || auth_token === '') {
-      return;
-    }
-      dispatch(fetchLoginFromToken(auth_token))
-  }
-
-  render(){
-    const { isAuthenticated, dispatch, children } = this.props
-    return (
-      <Navs
-        isAuthenticated={isAuthenticated}
-        onLogoutClick={() => dispatch(logoutUser())}
-        children={children} />
-    );
-  }
-}
+const Header = ({ isAuthenticated, dispatch, children }) => {  
+  return (
+    <Navs
+      isAuthenticated={isAuthenticated}
+      onLogoutClick={() => dispatch(logoutUser())}
+      children={children} />
+  );
+};
 
 const mapStateToProps = (state) => {
-  const {userAuth} = state
-  const {
-    isAuthenticated,
-  } = userAuth
-  return {
-    isAuthenticated
-  }
+  const { userAuth } = state
+  const { isAuthenticated } = userAuth
+
+  return { isAuthenticated }
 }
 
 export default connect(mapStateToProps)(Header)
