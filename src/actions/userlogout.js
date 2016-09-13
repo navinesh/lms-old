5853a29a819dd8f4557120e160bb4ef8f27efb6a
navinesh <navinesh.chand@gmail.com>
2016-09-13 12:19:13 +1200

    remove action types

--- src/actions/userlogout.js
+++ src/actions/userlogout.js
@@ -2,15 +2,11 @@ export const LOGOUT_USER_REQUEST = 'LOGOUT_USER_REQUEST'
 export const LOGOUT_USER_SUCCESS = 'LOGOUT_USER_SUCCESS'
 
 export const requestLogout = () => ({
-  type: LOGOUT_USER_REQUEST,
-  isFetching: true,
-  isAuthenticated: true
+  type: LOGOUT_USER_REQUEST
 });
 
 export const receiveLogout = () => ({
-  type: LOGOUT_USER_SUCCESS,
-  isFetching: false,
-  isAuthenticated: false
+  type: LOGOUT_USER_SUCCESS
 });
 
 export const logoutUser = () => {
