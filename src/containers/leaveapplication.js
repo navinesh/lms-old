802a7e0f0ebfe203d60a6a17f1175ace227a6090
navinesh <navinesh.chand@gmail.com>
2016-09-14 17:44:38 +1200

    add componentDidMount to fetch user details

--- src/containers/leaveapplication.js
+++ src/containers/leaveapplication.js
@@ -1,18 +1,38 @@
 import React, { Component } from 'react'
 import { browserHistory} from 'react-router'
 import { connect } from 'react-redux'
+import { fetchUserDetails } from '../actions/userdetails'
 import { fetchLeaveApplication } from '../actions/leaveapplication'
 import LeaveApplications from '../components/leaveapplication'
 
 class LeaveApplication extends Component {
+  componentDidMount() {
+    const { dispatch, auth_info } = this.props
+    const auth_token = auth_info.auth_token;
+    const user_id = auth_info.user_id;
+    if (auth_token || user_id) {
+      const userData = { auth_token: auth_token, user_id: user_id }
+      dispatch(fetchUserDetails(userData))
+    }
+    else {
+      const auth_token_local = localStorage.getItem('auth_token')
+      const user_id_local = localStorage.getItem('user_id')
+      if (auth_token_local || user_id_local) {
+        const userData = { auth_token: auth_token_local, user_id: user_id_local }
+        dispatch(fetchUserDetails(userData))
+      }
+    }
+  }
+
   render() {
-    const { dispatch, message, isAuthenticated, isFetching } = this.props
+    const { dispatch, message, isAuthenticated, isFetching, user_detail } = this.props
     return (
       <div className="LeaveApplication">
         {isAuthenticated &&
           <LeaveApplications
           isFetching={isFetching}
           message={message}
+          user_detail={user_detail}
           onLeaveApplicationClick={ applicationDetails => dispatch(fetchLeaveApplication(applicationDetails)) } />
         }
         {!isAuthenticated &&
@@ -24,14 +44,17 @@ class LeaveApplication extends Component {
 }
 
 const mapStateToProps = (state) => {
-  const { userAuth, leaveApplication } = state
-  const { isAuthenticated } = userAuth
+  const { userAuth, leaveApplication, userDetails } = state
+  const { isAuthenticated, auth_info } = userAuth
   const { isFetching, message} = leaveApplication
+  const { userDetail: user_detail } = userDetails
 
   return {
     message,
     isAuthenticated,
-    isFetching
+    isFetching,
+    auth_info,
+    user_detail
   }
 }
 
