dbf844881daea342a16ee9c0ed9bb622a0f16824
navinesh <navinesh.chand@gmail.com>
2016-09-16 14:20:38 +1200

    add space

--- src/containers/leaveapplication.js
+++ src/containers/leaveapplication.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { browserHistory} from 'react-router'
+import { browserHistory } from 'react-router'
 import { connect } from 'react-redux'
 import { fetchUserDetails } from '../actions/userdetails'
 import { fetchLeaveApplication } from '../actions/leaveapplication'
