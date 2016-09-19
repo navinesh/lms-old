a9f7a04575c9efca0f3d75641a65ed7e0ab23b43
navinesh <navinesh.chand@gmail.com>
2016-09-19 17:42:25 +1200

    delete module import

--- src/containers/userloginbox.js
+++ src/containers/userloginbox.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import { connect } from 'react-redux'
-import LeaveCalendarContainer from './leavecalendarcontainer'
 import UserLoginBox from './userlogincontainer'
 
 const MainUserLoginBox = ({ isAuthenticated }) => {
