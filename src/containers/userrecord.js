2eac6f64033be775c0a19404f1774981021a1c8c
navinesh <navinesh.chand@gmail.com>
2016-09-19 12:34:32 +1200

    format code

--- src/containers/userrecord.js
+++ src/containers/userrecord.js
@@ -9,7 +9,7 @@ class UserRecords extends Component {
   componentDidMount() {
     const { dispatch, auth_info } = this.props
     let auth_token = auth_info.auth_token;
-    if (auth_token) {
+    if(auth_token) {
       dispatch(fetchUserRecordIfNeeded(auth_token))
     }
     else {
