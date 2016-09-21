a910cb60e2ec465f2d0c10d2222d5303e0fbf7d9
navinesh <navinesh.chand@gmail.com>
2016-09-22 07:26:25 +1200

    disptach fetch user details

--- src/containers/userrecord.js
+++ src/containers/userrecord.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
-import { fetchUserRecordIfNeeded } from '../actions/userrecord'
+import { fetchUserDetailsIfNeeded } from '../actions/userdetails'
+import { fetchUserRecord, fetchUserRecordIfNeeded } from '../actions/userrecord'
 import UserRecord from '../components/userrecord'
 
 var Loader = require('halogen/PulseLoader');
@@ -10,31 +11,34 @@ class UserRecords extends Component {
     const { dispatch, auth_info } = this.props
     let auth_token = auth_info.auth_token;
     if(auth_token) {
-      dispatch(fetchUserRecordIfNeeded(auth_token))
+      dispatch(fetchUserDetailsIfNeeded(auth_token))
+      dispatch(fetchUserRecord(auth_token))
     }
     else {
       auth_token= localStorage.getItem('auth_token')
       if(auth_token) {
-        dispatch(fetchUserRecordIfNeeded(auth_token))
+        dispatch(fetchUserDetailsIfNeeded(auth_token))
+        dispatch(fetchUserRecord(auth_token))
       }
     }
   }
 
   render() {
-    const { isFetching, user_record, message } = this.props
+    const { isFetching, user_detail, user_record, message } = this.props
     return (
       <div className="UserDetailsContainer">
       {(isFetching ? <div className="offset-sm-5">
       <Loader color="#0275d8" size="12px" /></div> :
-      <UserRecord user_record={user_record} message={message} />)}
+      <UserRecord user_detail={user_detail} user_record={user_record} message={message} />)}
       </div>
     )
   }
 }
 
 const mapStateToProps = (state) => {
-  const {userRecords, userAuth} = state
+  const {userRecords, userAuth, userDetails} = state
   const { auth_info} = userAuth
+  const { userDetail: user_detail } = userDetails
   const {
     isFetching,
     userRecord: user_record,
@@ -45,7 +49,8 @@ const mapStateToProps = (state) => {
     isFetching,
     user_record,
     message,
-    auth_info
+    auth_info,
+    user_detail
   }
 }
 
